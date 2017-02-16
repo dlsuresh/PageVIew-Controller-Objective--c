cb52c67c9ff19647e06d28f81c99a900756b1a27
dlsuresh <dl.suresh6@gmail.com>
2017-02-16 14:47:42 +0530

    Initial Commit

---
+++ PageViewControllerDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PageViewControllerDemo
+//
+//  Created by Suresh on 2/16/17.
+//  Copyright © 2017 Suresh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
